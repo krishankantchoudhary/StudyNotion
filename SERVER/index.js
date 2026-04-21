260e0ed7bcb31b58fb4dc95354c7cf69878137d5
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2026-04-21 09:56:05 +0530

    Fix CORS for production

--- SERVER/index.js
+++ SERVER/index.js
@@ -24,12 +24,21 @@ dbConnect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+// app.use(
+//     cors({
+//         origin:"http://localhost:3000",
+//         credentials:true
+//     })
+// )
 app.use(
-    cors({
-        origin:"http://localhost:3000",
-        credentials:true
-    })
-)
+  cors({
+    origin: [ "http://localhost:3000", process.env.FRONTEND_URL ],
+     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+
 app.use(
     fileUpload({
         useTempFiles:true,

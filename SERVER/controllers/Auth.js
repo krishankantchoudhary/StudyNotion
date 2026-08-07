1d2ed8f6a6bbbf21e79ebf6ba9265b6c8330b35a
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2026-08-07 17:32:32 +0530

    otpsuccess

--- SERVER/controllers/Auth.js
+++ SERVER/controllers/Auth.js
@@ -56,17 +56,17 @@ exports.sendOTP = async (req, res) => {
 
     console.log("OTP Saved:", otpBody);
 
-    // Send Email
-    // await mailSender(
-    //   email,
-    //   "Verification Email",
-    //   otpTemplate(otp)
-    // );
+    //Send Email
     await mailSender(
-  email,
-  "Test Email",
-  "<h1>Hello from StudyNotion</h1>"
-);
+      email,
+      "Verification Email",
+      otpTemplate(otp)
+    );
+//     await mailSender(
+//   email,
+//   "Test Email",
+//   "<h1>Hello from StudyNotion</h1>"
+// );
 
     console.log("OTP Mail Sent Successfully");
 

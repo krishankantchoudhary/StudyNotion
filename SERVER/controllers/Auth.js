080a78d063d235b9495f7521a32729012638f57d
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2026-08-08 11:32:33 +0530

    df

--- SERVER/controllers/Auth.js
+++ SERVER/controllers/Auth.js
@@ -8,7 +8,7 @@ const Profile=require("../models/Profile");
 require("dotenv").config();
 const {passwordUpdated}=require("../mail/templates/passwordUpdate");
 
-//const otpTemplate = require("../mail/templates/emailVerificationTemplate");
+const otpTemplate = require("../mail/templates/emailVerificationTemplate");
 
 
 //sendotp for email verification
@@ -57,16 +57,16 @@ exports.sendOTP = async (req, res) => {
     console.log("OTP Saved:", otpBody);
 
     //Send Email
-    // await mailSender(
-    //   email,
-    //   "Verification Email",
-    //   otpTemplate(otp)
-    // );
     await mailSender(
-  email,
-  "Test Email",
-  "<h1>Hello from StudyNotion krishuu</h1>"
-);
+      email,
+      "Verification Email",
+      otpTemplate(otp)
+    );
+//     await mailSender(
+//   email,
+//   "Test Email",
+//   "<h1>Hello from StudyNotion krishuu</h1>"
+// );
 
     console.log("OTP Mail Sent Successfully");
 
